5b4ba711b2b2a9751618e0e7fa24406567718aae
Kirill <kirill.fedyanin@gmail.com>
2016-11-06 17:53:55 +0300

    Spritesheet animation

--- runner.js
+++ runner.js
@@ -93,17 +93,49 @@
    */
 
   function SpriteSheet(path, frameWidth, frameHeight){
-    this.img = new Image();
+    this.image = new Image();
     this.frameWidth = frameWidth;
     this.frameHeight = frameHeight;
 
     var self = this;
 
-    this.img.onload = function(){
+    this.image.onload = function(){
       self.framesPerRow = Math.floor(self.image.width / self.frameWidth);
     }
-    this.img.src = path;
+    this.image.src = path;
   }
 
+  /**
+   * Creates an animation from sprite
+   */
+  function Animation(spritesheet, frameSpeed, startFrame, endFrame){
+    var animationSequence = [];
+    var currentFrame = 0;
+    var counter = 0;
+
+    for (var frameNumber = startFrame; frameNumber <= endFrame; frameNumber++){
+      animationSequence.push(frameNumber);
+    }
+
+    this.update = function(){
+      if (counter = frameSpeed - 1){
+        currentFrame = (currentFrame + 1) % animationSequence.length;
+      }
+      counter = (counter + 1) % frameSpeed;
+    };
+
+    this.draw = function(x, y){
+      var row = Math.floor(animationSequence[currentFrame] / spritesheet.framesPerRow);
+      var col = Math.floor(animationSequence[currentFrame] % spritesheet.framesPerRow);
+
+      ctx.drawImage(
+        spritesheet.image,
+        col * spritesheet.frameWidth, row * spritesheet.frameHeight,
+        spritesheet.frameWidth, spritesheet.frameHeight,
+        x, y,
+        spritesheet.frameWidth, spritesheet.frameHeight
+      );
+    }
+  }
 
 })();
