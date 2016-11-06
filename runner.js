d7319070b45ecde81081c16083a4398116f401de
Kirill <kirill.fedyanin@gmail.com>
2016-11-06 19:37:59 +0300

    Key statuses

--- runner.js
+++ runner.js
@@ -256,6 +256,37 @@
     return player;
   })(Object.create(Vector.prototype));
 
+
+  var KEY_CODES = {
+    32: "space"
+  };
+
+  var KEY_STATUS = {};
+  for (var code in KEY_CODES){
+    if (KEY_CODES.hasOwnProperty(code)){
+      KEY_STATUS[KEY_CODES[code]] = false;
+    }
+  }
+
+  document.onkeydown = function(e) {
+    var keyCode = (e.keyCode) ? e.keyCode : e.charCode;
+    if (KEY_CODES[keyCode]) {
+      e.preventDefault();
+      KEY_STATUS[KEY_CODES[keyCode]] = true;
+    }
+  };
+  document.onkeyup = function(e) {
+    var keyCode = (e.keyCode) ? e.keyCode : e.charCode;
+    if (KEY_CODES[keyCode]) {
+      e.preventDefault();
+      KEY_STATUS[KEY_CODES[keyCode]] = false;
+    }
+  };
+
+
+
+
+
   function startGame(){
 
     for (i = 0, length = Math.floor(canvas.width / platformWidth) + 1; i < length; i++) {
