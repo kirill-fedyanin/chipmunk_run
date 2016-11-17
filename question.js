aa468de8c813b15880b6ee4f5c29ec8a5f1bd50e
Kirill <kirill.fedyanin@gmail.com>
2016-11-17 17:06:56 +0300

    Few options for wrong answer response

--- question.js
+++ question.js
@@ -1,12 +1,21 @@
 $(function(){
   var answers = ["внутри", "со мной", "с собой"];
+  var wrongs = [
+    "Хм, интересно, но не то. Попробуй ещё разок.",
+    "Не то, подумай ещё. Регистр слов значения не имеет, кстати.",
+    "Ну же, ты знаешь ответ. Это был не он сейчас.",
+    "Серьёзно? Нет, нужно другое",
+  ]
   $("form").submit(function(event){
     event.preventDefault();
     var answer = $("#answer").val().toLowerCase();
-    var right = answers.indexOf(answer) != -1;
-    if (right){
+    if (answers.indexOf(answer) >= 0){
+      $("#wrong-response").hide();
       $("#right-response").show();
     } else {
+      $("#right-response").hide();
+      var response = wrongs[Math.floor(Math.random()*wrongs.length)];
+      $("#wrong-response").html(response);
       $("#wrong-response").show();
     }
   });
